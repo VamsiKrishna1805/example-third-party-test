3f6b1f15fba52b1622bc1783f049e0873aa245d6
vamsikrishna1805 <vamsi.krishna@intimetec.com>
2022-04-26 16:46:00 +0530

    added KountDataCollector as a dependency

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "KountAnalyticsPackage", url: "https://github.com/VamsiKrishna1805/example-kount-package", from: "1.0.3")
+        .package(name: "KountDataCollectorPackage", url: "https://github.com/Kount/kount-ios-swift-package", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ThirdPartyPackage",
-            dependencies: ["KountAnalyticsPackage"]),
+            dependencies: ["KountDataCollectorPackage"]),
         .testTarget(
             name: "ThirdPartyPackageTests",
             dependencies: ["ThirdPartyPackage"]),
